99e082c24625f2a82f542428b6e44556cdc70fbd
mmyslblocky <michal.myslinski@blockydevs.com>
2024-09-18 13:23:18 +0200

    removed remnants of view logs functionality

--- client/modules/globalVariables.ts
+++
@@ -1,9 +0,0 @@
-let _canisterLogs: { [key: string]: string } = {};
-
-export function setCanisterLogs(logs: { [key: string]: string }) {
-    _canisterLogs = logs;
-}
-
-export function getCanisterLogs(): { [key: string]: string } {
-    return _canisterLogs;
-}
